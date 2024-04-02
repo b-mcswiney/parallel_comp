8c87aa5a1ffa8fe5d27e6a704d41c08567a57e99
Brooklyn Mcswiney <ed20b3m@leeds.ac.uk>
2024-04-02 18:13:33 +0100

    fixed bug with serial and parallel being the same. Need to fix read write memeory

--- cwk3/cwk3.c
+++ cwk3/cwk3.c
@@ -28,7 +28,7 @@ int main( int argc, char **argv )
 	cl_command_queue queue = clCreateCommandQueue( context, device, 0, &status );
 	
 	// Get the parameters (N = no. of nodes/gradients, M = no. of inputs). getCmdLineArgs() is in helper_cwk.h.
-	int N, M;
+	int N, M, i, j;
 	getCmdLineArgs( argc, argv, &N, &M );
 
 	// Initialise host arrays. initialiseArrays() is defined in helper_cwk.h. DO NOT REMOVE or alter this routine;
@@ -36,9 +36,15 @@ int main( int argc, char **argv )
 	float
 		*gradients = (float*) malloc( N  *sizeof(float) ),
 		*inputs    = (float*) malloc(   M*sizeof(float) ),
-		*weights   = (float*) malloc( N*M*sizeof(float) );
+		*weights   = (float*) malloc( N*M*sizeof(float) ),
+		*weights_serial = (float*) malloc(N*M*sizeof(float));
 	initialiseArrays( gradients, inputs, weights, N, M );			// DO NOT REMOVE.
 	
+	// copy weights over for checking later
+	for( i=0; i < N*M; i++ ){
+		weights_serial[i] = weights[i];
+		printf("%f = %f\n",  weights_serial[i], weights[i]);
+	}
 	
 	//
 	// Implement the GPU solution to the problem.
@@ -47,8 +53,8 @@ int main( int argc, char **argv )
 	// Allocate memory for the arrays
 	cl_mem device_gradients = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, N*sizeof(float), gradients, &status);
 	cl_mem device_inputs = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, M*sizeof(float), inputs, &status);
-	cl_mem device_weights = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, M*N*sizeof(float), weights, &status);
-	
+	cl_mem device_weights = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, M*N*sizeof(float), weights, &status); // Need to make read/write (currently can only read	
+
 	//
 	// Perform calculation on the GPU
 	//
@@ -60,10 +66,11 @@ int main( int argc, char **argv )
 	status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &device_gradients);
 	status = clSetKernelArg(kernel, 1, sizeof(cl_mem), &device_inputs);
 	status = clSetKernelArg(kernel, 2, sizeof(cl_mem), &device_weights);
+	status = clSetKernelArg(kernel, 3, sizeof(cl_int), &M);
 
 	// Get maximum work group size for device
 	size_t maxWorkItems;
-	glGetDeviceInfo(device, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(size_t), &maxWorkItems, NULL);
+	clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(size_t), &maxWorkItems, NULL);
 
 	// Set up global problem size and the work group size
 	size_t indexSpaceSize[1], workGroupSize[1];
@@ -80,16 +87,41 @@ int main( int argc, char **argv )
 	// Get result from device to host
 	status = clEnqueueReadBuffer(queue, device_weights, CL_TRUE, 0, N*M*sizeof(float), weights, 0, NULL, NULL);
 	
-	
+	// Check results
+	printf("Checking (Will only display weights if correct)\n");
+	for(i=0; i<N; i++) {
+		for(j=0; j<M; j++){
+			weights_serial[i*M+j] += gradients[i] * inputs[j];
+		}
+	}
+
+	for(i=0; i<N*M; i++) {
+		printf("Comparing:(serial) %f and (GPU) %f\n", weights_serial[i], weights[i]);
+		if(weights_serial[i] != weights[i]) {
+			printf("Calculation Failed\n");
+			break;
+		}
+	}
+				
 
 	// Output result to screen. DO NOT REMOVE THIS LINE (or alter displayWeights() in helper_cwk.h); this will be replaced
 	// with a different displayWeights() for the the assessment, so any changes you might make will be lost.
 	displayWeights( weights, N, M) ;								// DO NOT REMOVE.
 	
-	free( gradients );
-	free( inputs    );
-	free( weights   );
+	//
+	// Clean up
+	//
 
+	clReleaseMemObject( device_gradients );
+	clReleaseMemObject( device_inputs    );
+	clReleaseMemObject( device_weights   );
+
+	free( gradients      );
+	free( inputs         );
+	free( weights        );
+	free( weights_serial );
+
+	clReleaseKernel      ( kernel  );
 	clReleaseCommandQueue( queue   );
 	clReleaseContext     ( context );
 
--- cwk3/cwk3.cl
+++ cwk3/cwk3.cl
@@ -1,3 +1,13 @@
 // Implement the kernel (or kernels) for coursework 3 in this file.
 
-void
+__kernel
+void  weightsUpdate(__global float *gradients, __global float *inputs, __global float *weights, __constant int *device_M)
+{
+	// Global id tells us the index for this thread
+	int gid = get_global_id(0);
+	int M = *device_M;
+	
+	// Perform the weights editing loop
+	int j;
+	for( j=0; j<M; j++) weights[gid*M+j] += gradients[gid] * inputs[j];
+}
